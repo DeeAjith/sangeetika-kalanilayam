e4f3db4d643bee5a559f2885488477a9ec42f38a
DeeAjith <deevannagari@gmail.com>
2021-08-13 00:41:38 +0530

    Updates

---
+++ assets/js/c.js
@@ -0,0 +1,19 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwZCX5riqSgE9McEWfweoc1nAy7s88kLiIqk7SDxjg4Jh4U-0s/exec'
+const formId = $('#contact-form')
+formId.submit((e)=>{
+    e.preventDefault()
+    $.ajax({
+        url:scriptURL,
+        data:formId.serialize(),
+        method:"post",
+        success:function (response){
+            alert("Form submitted successfully")
+            window.location.reload()
+            //window.location.href="https://google.com"
+        },
+        error:function (err){
+            alert("Something Error, Try again later!")
+
+        }
+    })
+})
